cc2fd44e79880f523a9d33d8ecd59e8598b53614
Mikhailuu <mihiluu@gmail.com>
2025-06-06 17:16:42 +0800

    like popup

--- src/components/ConfirmPopup/ConfirmPopup.jsx
+++ src/components/ConfirmPopup/ConfirmPopup.jsx
@@ -1,17 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import classes from "./ConfirmPopup.module.scss";
 
-const ConfirmPopup = ({ messages, onConfirm, onCancel }) => {
+const ConfirmPopup = ({ messages, isLogin = false, onConfirm, onCancel }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/sign-in");
+  };
   return (
     <div className={classes["popup-overlay"]}>
       <div className={classes.popup}>
         <p>{messages}</p>
         <div className={classes.buttons}>
-          <button onClick={onConfirm} className={classes["confirm-button"]}>
-            Да
-          </button>
-          <button onClick={onCancel} className={classes["cancel-button"]}>
-            Нет
-          </button>
+          {onConfirm && onCancel && (
+            <>
+              <button onClick={onConfirm} className={classes["confirm-button"]}>
+                Да
+              </button>
+              <button onClick={onCancel} className={classes["cancel-button"]}>
+                Нет
+              </button>
+            </>
+          )}
+          {isLogin && (
+            <button onClick={handleClick} className={classes["cancel-button"]}>
+              go to Sign up
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -2,13 +2,24 @@ import classes from "./LikeButton.module.scss";
 
 import { useState } from "react";
 import { likeArticle, unLikeArticle } from "../../api/fetchApi";
+import { useSelector } from "react-redux";
+import ConfirmPopup from "../ConfirmPopup/ConfirmPopup";
 
 const LikeButton = ({ initialLikes = 0, initialLiked = false, slug }) => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const [showPopup, setShowPopup] = useState(false);
+
   const [likes, setLikes] = useState(initialLikes);
   const [isLiked, setIsLiked] = useState(initialLiked);
 
   const handleLike = async (e) => {
     e.stopPropagation();
+
+    if (!isAuthenticated) {
+      setShowPopup(true);
+      return;
+    }
+
     try {
       if (isLiked) {
         await unLikeArticle(slug);
@@ -25,17 +36,23 @@ const LikeButton = ({ initialLikes = 0, initialLiked = false, slug }) => {
   };
 
   return (
-    <label
-      className={`${classes["like-button"]} ${isLiked ? classes.active : ""}`}
-    >
-      <input
-        type="checkbox"
-        onClick={handleLike}
-        className={classes["like-input"]}
-        defaultChecked={isLiked}
-      />
-      <span className={classes["like-span"]}>{likes}</span>
-    </label>
+    <>
+      <label
+        className={`${classes["like-button"]} ${isLiked ? classes.active : ""}`}
+      >
+        <input
+          type="checkbox"
+          onClick={handleLike}
+          className={classes["like-input"]}
+          defaultChecked={isLiked}
+        />
+        <span className={classes["like-span"]}>{likes}</span>
+      </label>
+
+      {showPopup && (
+        <ConfirmPopup messages={"Log in required"} isLogin={true} />
+      )}
+    </>
   );
 };
 
