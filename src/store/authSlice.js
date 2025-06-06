9efe5666a6bc1ed7b4bb6dedac8acd929198641d
Mikhailuu <mihiluu@gmail.com>
2025-06-06 15:27:01 +0800

    fix authSlice

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
-  token: null,
-  isAuthentiated: false,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  token: localStorage.getItem("token") || null,
+  isAuthentiated: !!localStorage.getItem("token") || false,
 };
 
 const authSlice = createSlice({
@@ -15,12 +15,14 @@ const authSlice = createSlice({
       state.user = user;
       state.token = token;
       state.isAuthentiated = true;
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
       localStorage.setItem("token", token);
     },
     logout: (state) => {
       state.user = null;
       state.token = null;
       state.isAuthentiated = false;
+      localStorage.removeItem("user");
       localStorage.removeItem("token");
     },
   },
